e1d95ec720dbe983b5598dd27b1e4c488f0bc548
BACKUP2K07 <backup2k07@gmail.com>
2025-07-03 21:08:54 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+export default {
+  "botName": "𝐍𝐎𝐎𝐁𝐗 𝐁𝐎𝐓 🤍🦋",
+  "prefix": "-",
+  "nickname": "Noobxbot",
+  "adminIds": [
+    "61569568560258",
+    "",
+    "",
+    ""
+  ],
+  "facebookAccount": {
+    "email": "bultelakku@gufum.com",
+    "password": "asta@0123"
+  },
+  "logging": {
+    "messageObjects": true
+  },
+  "language": "en",
+  "autoRestart": {
+    "time": null
+  },
+  "optionsFca": {
+    "forceLogin": true,
+    "listenEvents": true,
+    "updatePresence": true,
+    "autoMarkDelivery": true,
+    "autoMarkRead": true,
+    "userAgent": "Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Mobile Safari/537.36"
+  }
+}
